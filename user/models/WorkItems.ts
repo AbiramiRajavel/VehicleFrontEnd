660165665a94a0a4e3ba48d1a9a4f66dcdda18e1
Abirami <77911777+AbiramiRajavel@users.noreply.github.com>
2022-09-15 13:07:03 +0530

    Add files via upload

---
+++ user/models/WorkItems.ts
@@ -0,0 +1,15 @@
+export class WorkItems {
+    userId!: number;
+    workitemId!: number;
+    itemName!: string;
+    itemType!: string;
+    itemDescription!: string;
+    messageToRecipient!: string;
+    quantity!: string;
+    sourceCountry!: string;
+    destinationCountry!: string;
+    originHarborLocation!: string;
+    selectedHarborLocations!: string;
+    shippingDate!: Date;
+    amount!: number;
+}
